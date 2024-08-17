64dfe8e0e449f3fe9fa37cb6ec4c9b1f58cec7b0
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-08-17 12:42:37 -0300

    Modal Error Catch

--- src/pages/Contact/ContactForm.tsx
+++ src/pages/Contact/ContactForm.tsx
@@ -2,13 +2,13 @@
 import { useState } from 'react';
 import { Button, Spinner, useDisclosure } from '@nextui-org/react';
 import { Modal, ModalContent, ModalBody } from '@nextui-org/react';
-import { FaCheckCircle } from 'react-icons/fa';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 import emailjs from 'emailjs-com';
 
 import Input from '@/components/Input';
 
 export default function ContactForm() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,6 +18,7 @@ export default function ContactForm() {
 
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [loading, setLoading] = useState(false);
+  const [modalContent, setModalContent] = useState<React.ReactNode>(null);
 
   const validate = () => {
     const newErrors: { [key: string]: string } = {};
@@ -56,13 +57,30 @@ export default function ContactForm() {
       emailjs
         .send(serviceID, templateID, formData, publicKey)
         .then((response) => {
+          setModalContent(
+            <>
+              <FaCheckCircle className="text-emerald-500 text-8xl mb-4 rounded-full" />
+              <h3 className="text-sm text-gray-100">
+                Message sent successfully!
+              </h3>
+            </>
+          );
           console.log('Email sent successfully:', response);
           setLoading(false);
           onOpen();
         })
         .catch((error) => {
+          setModalContent(
+            <>
+              <FaTimesCircle className="text-red-500 text-8xl mb-4 rounded-full" />
+              <h3 className="text-sm text-gray-100">
+                Failed to send the message. Please try again later.
+              </h3>
+            </>
+          );
           console.error('Error sending email:', error);
           setLoading(false);
+          onOpen();
         });
     }
   };
@@ -111,20 +129,13 @@ export default function ContactForm() {
       </Button>
       <Modal
         isOpen={isOpen}
-        onOpenChange={onOpenChange}
+        onClose={onClose}
         className="bg-slate-950 border border-gray-800"
       >
         <ModalContent className="py-9">
-          {(onClose) => (
-            <>
-              <ModalBody className="flex flex-col items-center justify-center text-center">
-                <FaCheckCircle className="text-emerald-500 text-8xl mb-4 rounded-full" />
-                <h3 className=" text-sm text-gray-100">
-                  Message sent successfully!
-                </h3>
-              </ModalBody>
-            </>
-          )}
+          <ModalBody className="flex flex-col items-center justify-center text-center">
+            {modalContent}
+          </ModalBody>
         </ModalContent>
       </Modal>
     </form>
