158c8802cef066954b3cf0d5b69696fb70c11bee
Monza <matiasmonzalvoarg@gmail.com>
2024-09-02 19:35:37 -0300

    SEO aplicado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+// layout.tsx
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -6,16 +7,43 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Mazzo',
-  description: 'Mazzo Developments'
+  description: 'Mazzo Developments',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'Mazzo Developments',
+    description: 'Leading developments in tech and innovation.',
+    url: 'https://www.mazzodevelopments.com',
+    siteName: 'Mazzo',
+    images: [
+      {
+        url: '/static/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Mazzo Developments',
+      },
+    ],
+    type: 'website',
+    locale: 'en_US',
+  },
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Mazzo Developments" />
+        <meta name="keywords" content="development, technology, innovation, software, ai" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
