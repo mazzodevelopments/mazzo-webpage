e17dded4061be2cc4f0dc6703d56b1fa4f1819b7
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-09-03 11:23:56 -0300

    Metadata fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.mazzodevelopments.com'), 
   title: 'Mazzo',
   description: 'Mazzo Developments',
   icons: {
