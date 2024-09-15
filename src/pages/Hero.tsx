5c180c70b52f1f90bbfd6915090fb15a64a19b55
Tomás Matteozzi <tomasmatteozzi@gmail.com>
2024-09-15 19:32:27 -0300

    Hero V2

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
             linear-gradient(0deg, transparent 24%, rgba(255, 255, 255, 0.1) 25%, rgba(255, 255, 255, 0.1) 26%, transparent 27%, transparent 74%, rgba(255, 255, 255, 0.1) 75%, rgba(255, 255, 255, 0.1) 76%, transparent 77%, transparent),
             linear-gradient(90deg, transparent 24%, rgba(255, 255, 255, 0.1) 25%, rgba(255, 255, 255, 0.1) 26%, transparent 27%, transparent 74%, rgba(255, 255, 255, 0.1) 75%, rgba(255, 255, 255, 0.1) 76%, transparent 77%, transparent)`,
           backgroundSize: '55px 55px',
-          opacity: 0.35,
+          opacity: 0.2,
           zIndex: 1
         }}
       />
